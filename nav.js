6356b701c207138e7767c1446bb609488b4a18f8
MosesMuiru <muirumoses9@gmail.com>
2023-05-19 00:15:41 +0300

    add home page button

--- nav.js
+++ nav.js
@@ -2,6 +2,13 @@
 const openBtn = document.getElementById("openBtn")
 const closeBtn = document.getElementById("closeBtn")
 
+const landingpageInfo = document.getElementById("landingpage-info")
+
+// the button at the navigation bar
+const one = document.getElementById("one")
+const two = document.getElementById("two")
+const three = document.getElementById("three")
+
 const mobileList = document.getElementById("mobileList")
 openBtn.addEventListener("click", () => {
     console.log("btn clicked")
@@ -14,5 +21,30 @@ closeBtn.addEventListener("click", () => {
     console.log("clicked")
     mobileList.classList.toggle("hidden")
     openBtn.classList.toggle("hidden")
+   
+    
+
 
 })
+
+console.log("the nav is working")
+
+// setting up the buttons when clicked
+one.addEventListener("click",() => {
+    console.log("this btn has been clicked")
+    secondinfo.classList.add("hidden")
+    landingpageInfo.classList.remove("hidden")
+    two.classList.remove("clicked")
+    one.classList.add("clicked")
+
+})
+
+two.addEventListener("click",() => {
+    console.log("this tw has been clicked")
+    two.classList.add("clicked")
+    one.classList.remove("clicked")
+    landingpageInfo.classList.add("hidden")
+     secondinfo.classList.remove("hidden")
+})
+
+
